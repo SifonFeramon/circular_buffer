d528bac895ea4a9e014f2ea937c6764bb871d07c
SifonFeramon <truesmilesifon@gmail.com>
2022-04-17 23:21:24 +0300

    add function for class

--- circular_buffer.h
+++ circular_buffer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdio>
+#include <mutex>
 
 template<class T>
 class circular_buffer
@@ -11,15 +12,77 @@ public:
 		_write_index = 0;
 		_read_index = 0;
 		_count_elements = iSize;
+		_isfull = false;
 	};
 
 	std::size_t size() const;
 	void push(T element);
 	T pop();
+	T& operator[](const std::size_t index);
+	const T& operator[](const std::size_t index) const;
+	~circular_buffer() { delete[] _buffer; }
 private:
 	T *_buffer;
 	std::size_t _write_index;
 	std::size_t _read_index;
 	std::size_t _count_elements;
+	std::mutex _lock;
+	bool _isfull;
 };
+
+template <class T>
+std::size_t circular_buffer<T>::size() const
+{
+	if (_isfull)
+		return _count_elements;
+
+	return (_write_index + _count_elements - _read_index) % _count_elements;
+}
+
+template <class T>
+void circular_buffer<T>::push(T element)
+{
+	std::lock_guard<std::mutex> lock(_lock);
+
+	_buffer[_write_index++] = element;
+	if (_write_index == _count_elements)
+	{
+		_write_index = 0;
+		_isfull = true;
+	}
+}
+
+template <class T>
+T circular_buffer<T>::pop()
+{
+	std::lock_guard<std::mutex> lock(_lock);
+
+	if (_read_index == _count_elements)
+		_read_index = 0;
+
+	if (_isfull)
+		_isfull = !_isfull;
+
+	return _buffer[_read_index++];
+}
+
+template <class T>
+T& circular_buffer<T>::operator[](const std::size_t index)
+{
+	std::lock_guard<std::mutex> lock(_lock);
+	if (_read_index == _write_index)
+		return T();
+
+	return _buffer[(_read_index + index) % _count_elements];
+}
+
+template<class T>
+const T& circular_buffer<T>::operator[](const std::size_t index) const
+{
+	std::lock_guard<std::mutex> lock(_lock);
+	if (_read_index == _write_index)
+		return T();
+
+	return _buffer[(_read_index + index) % _count_elements];
+}
 
--- main.cpp
+++ main.cpp
@@ -3,5 +3,18 @@
 int main(int argc, char* argv[])
 {
 	printf("path %s", argv[0]);
+	circular_buffer<int> que(10);
+
+	for (std::size_t i = 0; i < 10; ++i)
+	{
+		que.push(i);
+	}
+
+	int len = que.size()+1;
+	for (std::size_t i = 0; i < len; ++i)
+	{
+		printf("element [%ld] = %d\n", i, que.pop());
+	}
+
 	return 0;
 }
